e2189fcf94ceca45a1f20688f64c08db7ddb91bc
Anubhav <106881580+its-me-anubhav@users.noreply.github.com>
2022-11-11 15:13:28 +0530

    Create half_subtactor.cpp

---
+++ half_subtactor.cpp
@@ -0,0 +1,19 @@
+// C++ program to implement Half subtractor
+   #include <bits/stdc++.h>
+   using namespace std;
+
+   void Half_Subtractor(int A,int B){
+   int Difference,Borrow;
+   Difference = A ^ B;
+    A = not(A);
+    Borrow = A & B;
+     // printing the value
+    cout<<"Difference = "<<Difference<<endl;
+    cout<<"Borrow = "<<Borrow<<endl;
+}
+int main() {
+    int A = 1;
+    int B = 0;
+  Half_Subtractor(A,B);
+    return 0;
+}
